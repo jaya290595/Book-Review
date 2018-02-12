620136d973797f598504f1f75b733e1ada7c11b0
jayapanday <jaya@maropost.com>
2018-02-12 20:54:25 +0530

    Users reviews and rating

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,47 @@
+class ReviewsController < ApplicationController
+  before_action :find_book
+  before_action :find_review, only: [:edit, :update, :destroy]
+
+	def new
+    @review = Review.new
+  end
+
+  def create
+    @book= Book.find(params[:book_id])
+    @review= @book.reviews.create(review_params)
+    @review.user_id = current_user.id
+      if @review.save
+        redirect_to book_path(@book)
+      else
+        render 'new'
+      end
+
+      def edit
+      end
+
+      def update
+        if @review.update(review_params)
+          redirect_to book_path(@book)
+        else
+          render 'edit'
+        end
+
+      end
+end
+
+  private
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
+
+    def find_book
+      @book=Book.find(params[:book_id])
+    end
+
+    def find_review
+      @review = Review.find(params[:id])
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do	
   devise_for :users
-resources :books
-root 'books#index'
-
-
+  resources :books do
+	resources :reviews
+  end
+  root 'books#index'
 end
